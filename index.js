b5c346df5949c7d4de7daf6759d887e81447a127
Gakuto Watanabe <g_watanabe@ga-tech.co.jp>
2022-07-10 20:22:48 +0900

    event demo

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const Person = require("./person");
+const Logger = require('./logger')
 
-const person1 = new Person('John', 31)
+const logger = new Logger()
 
-console.log(person1);
+logger.on('message', (data) => console.log('Called Listner:', data))
+
+logger.log('Hello!!')
